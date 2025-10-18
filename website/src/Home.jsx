72ff6bd9139647c21b155e5a0643b607c3aeb8b9
lacyelln <lacyellenmiller@gmail.com>
2025-10-18 09:00:38 -0600

    added styling changes to home page

--- website/src/Home.jsx
+++ website/src/Home.jsx
@@ -14,18 +14,11 @@ function Home() {
         <h1>Welcome to BYU!</h1>
         </div>
         <div className='overlay'>
-        <p>Congratulations and welcome to Brigham Young University — 
-          a place where faith, intellect, and friendship come together 
-          in a unique and powerful way. As a new Cougar, you’re now 
-          part of a vibrant community committed to lifelong learning, 
-          service, and becoming your best self. Your time here will be 
-          filled with discovery — both academic and personal — as you build 
-          meaningful relationships, grow spiritually, and pursue excellence 
-          in everything you do. We’re excited to see how you’ll contribute your 
-          talents, your voice, and your light to the BYU family. This is just 
-          the beginning of something incredible. This webpage is designed to help you
-          in both your academic and social endeavors, feel free to look around and
-          welcome home.</p>
+        <p>Congratulations and welcome to your first year at Brigham Young University!
+          We hope this webpage will help you connect with help with other students for all your social and academic needs!
+          Feel free to post events whenever you want to host your own social or academic event or let people know what's going on. Feel free as well to 
+          look around for some activities that might interest you! 
+          Good luck this first year! You got it!</p>
           </div>
       </div>
       
