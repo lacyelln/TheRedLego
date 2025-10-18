9aabb731f3ec06ae2f33ee1b1aa21a88efa96323
Jack Johnson <jrj221@byu.edu>
2025-10-18 02:41:44 -0600

    added makePost

--- website/src/makePost.jsx
+++ website/src/makePost.jsx
@@ -1,5 +1,39 @@
 import React from 'react'
 
 export function MakePost() {
-    
+    const [eventName, setEventName] = React.useState("");
+    const [eventDesc, setEventDesc] = React.useState("");
+    const [eventDate, setEventDate] = React.useState("");
+    const [eventTime, setEventTime] = React.useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // ✅ prevent page reload
+
+        const data = {
+        eventName,
+        eventDesc,
+        eventDate,
+        eventTime,
+        };
+
+        fetch("/api/event/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+              },
+            body: JSON.stringify(data),
+        });
+    }
+
+    return (
+        <>
+            <form id="eventForm" onSubmit={handleSubmit}>
+                <input type="text" name="Event Name" placeholder="Event Name" value={eventName} onChange={(e) => {setEventName(e.target.value)}} required />
+                <input type="text" name="Event Desc" placeholder="Event Desc" value={eventDesc} onChange={(e) => {setEventDesc(e.target.value)}} required />
+                <input type="text" name="Event Date" placeholder="Event Date" value={eventDate} onChange={(e) => {setEventDate(e.target.value)}} required />
+                <input type="text" name="Event Time" placeholder="Event Time" value={eventTime} onChange={(e) => {setEventTime(e.target.value)}} required />
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
 }
